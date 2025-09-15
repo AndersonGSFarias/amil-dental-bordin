e845d8c42764b120953941e9f23dd32cf2225abf
Anderson <anderson.gfarias05@gmail.com>
2025-09-15 19:10:30 -0300

    Atualização:Troca do formato das imagens para otimizaçãop

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,5 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
-  daisyui: {
-    themes: [
-      {
-        bordin: {
-          primary: "#29196d",
-          secondary: "#4f14ff",
-          accent: "#00adff",
-          neutral: "#f1edff",
-          "base-100": "#ffffff",
-          // outas chaves que você precise...
-        },
-      },
-    ],
-  },
+  daisyui: {},
 };
